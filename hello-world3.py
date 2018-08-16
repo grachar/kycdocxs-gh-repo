7e47c42a590bd51b7885f7865876327ed2a0ed5b
Mark Thomas <Mark.Thomas@grachar.co.uk>
2018-08-16 15:16:09 +0100

    Confirmed

--- hello-world3.py
+++ hello-world3.py
@@ -3,6 +3,5 @@ print "Hello Again"
 print "I like typing this."
 print "This is fun-not."
 print 'Yay! Printing.'
-print "I'd much rather you 'not'."
 print "I'd much rather you ssss'."
 print 'I "said" do not touch this.'
