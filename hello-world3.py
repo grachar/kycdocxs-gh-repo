b92b8dc480adea1fef6764e57c3f85f93bf75545
Mark Thomas <Mark.Thomas@grachar.co.uk>
2018-08-17 09:25:54 +0100

    Purge

--- hello-world3.py
+++
@@ -1,7 +0,0 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun-not."
-print 'Yay! Printing.'
-print "I'd much rather you ssss'."
-print 'I "said" do not touch this.'
--- hello-world4.py
+++
@@ -1,7 +0,0 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun-not."
-print 'Yay! Printing.'
-print "I'd much rather you'."
-print 'I "said" do not touch this.'
