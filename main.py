8617c6b2e0c6241cca0f652d9fcfd184f4e426a9
Mark Thomas <Mark.Thomas@grachar.co.uk>
2018-08-22 10:33:53 +0100

    Lots of changes

--- main.py
+++ main.py
@@ -12,15 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import webapp2
 import webapp2_extras
+
+import cgi
+
+from google.appengine.api import users
+import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World change x6')
+        self.response.out.write("""
+          <html>
+            <body>
+              <form action="/sign" method="post">
+                <div><textarea name="content" rows="3" cols="60"></textarea></div>
+                <div><input type="submit" value="Sign Guestbook"></div>
+              </form>
+            </body>
+          </html>""")
 
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
 
-app = webapp2.WSGIApplication([
+application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/sign', Guestbook)
 ], debug=True)
+
+def main():
+    application.run()
+
+if __name__ == "__main__":
+    main()
