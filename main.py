52f92a01ec9ec723232cde9a1dcfbc80ba4c7874
Mark Thomas <Mark.Thomas@grachar.co.uk>
2018-08-22 10:41:52 +0100

    Revert

--- main.py
+++ main.py
@@ -12,38 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import webapp2_extras
-
-import cgi
-
-from google.appengine.api import users
 import webapp2
+import webapp2_extras
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write("""
-          <html>
-            <body>
-              <form action="/sign" method="post">
-                <div><textarea name="content" rows="3" cols="60"></textarea></div>
-                <div><input type="submit" value="Sign Guestbook"></div>
-              </form>
-            </body>
-          </html>""")
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World change x6')
 
-class Guestbook(webapp2.RequestHandler):
-    def post(self):
-        self.response.out.write('<html><body>You wrote:<pre>')
-        self.response.out.write(cgi.escape(self.request.get('content')))
-        self.response.out.write('</pre></body></html>')
 
-application = webapp2.WSGIApplication([
+app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', Guestbook)
 ], debug=True)
-
-def main():
-    application.run()
-
-if __name__ == "__main__":
-    main()
